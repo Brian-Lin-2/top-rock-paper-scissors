e7730627e84d40c21d933c2105a0c2812b31f819
Brian <linbrian24@gmail.com>
2023-03-06 23:27:17 -0500

    Create base rock paper scissors simulation

---
+++ program.js
@@ -0,0 +1,72 @@
+let getComputerChoice = () => {
+    const choice = ["Rock", "Paper", "Scissors"];
+
+    // Random choice between 0-2.
+    // Floor to make sure its random.
+    return choice[Math.floor(Math.random() * 3)];
+}
+
+let playRound = (player, computer) => {
+    switch (computer) {
+        case "Rock":
+            if (player.toLowerCase() === "rock") {
+                return "Tie!";
+            }
+
+            else if (player.toLowerCase() === "paper") {
+                return "You Win! Paper beats Rock";
+            }
+
+            else if (player.toLowerCase() === "scissors") {
+                return "You Lose! Rock beats Scissor";
+            }
+
+            else {
+                return "Invalid Option.";
+            }
+
+            break;
+
+        case "Paper":
+            if (player.toLowerCase() === "rock") {
+                return "You Lose! Paper beats Rock";
+            }
+
+            else if (player.toLowerCase() === "paper") {
+                return "Tie!";
+            }
+
+            else if (player.toLowerCase() === "scissors") {
+                return "You Win! Scissor beats Paper";
+            }
+
+            else {
+                return "Invalid Option.";
+            }
+
+            break;
+
+        case "Scissor":
+            if (player.toLowerCase() === "rock") {
+                return "You Win! Rock beats Scissors";
+            }
+
+            else if (player.toLowerCase() === "paper") {
+                return "You Lose! Scissors beats Paper";
+            }
+
+            else if (player.toLowerCase() === "scissors") {
+                return "Tie!";
+            }
+
+            else {
+                return "Invalid Option.";
+            }
+
+            break;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
